6e4729c707db65e941ae670bfef9124a6b6fba1f
Gabriel <gpena@untref.edu.ar>
2026-05-02 20:29:02 -0300

    A little improvements to avoid hardcode offsets in tests

--- tests/tests_link_layer/test_link_layer.py
+++ tests/tests_link_layer/test_link_layer.py
@@ -130,7 +130,8 @@ class TestChecksumCalculation:
 
     def test_checksum_changes_with_is_last(self, example_link_layer, base_body):
         body_with_islast_0 = base_body.copy()
-        body_with_islast_0[2] ^= 1  # flip is_last
+        is_last_idx = example_link_layer.seq_size
+        body_with_islast_0[is_last_idx] ^= 1  # flip is_last
         cs1 = example_link_layer._compute_checksum(base_body)
         cs2 = example_link_layer._compute_checksum(body_with_islast_0)
 
@@ -138,7 +139,8 @@ class TestChecksumCalculation:
 
     def test_checksum_changes_with_is_ack(self, example_link_layer, base_body):
         body_with_isack_1 = base_body.copy()
-        body_with_isack_1[3] ^= 1  # flip is_ack
+        is_ack_idx = example_link_layer.seq_size + 1
+        body_with_isack_1[is_ack_idx] ^= 1  # flip is_ack
         cs1 = example_link_layer._compute_checksum(base_body)
         cs2 = example_link_layer._compute_checksum(body_with_isack_1)
 
@@ -146,7 +148,8 @@ class TestChecksumCalculation:
 
     def test_checksum_changes_with_real_length(self, example_link_layer, base_body):
         body_with_different_real_length = base_body.copy()
-        body_with_different_real_length[4] ^= 1  # flip a bit from real_length
+        real_length_idx = example_link_layer._get_header_size() - 1
+        body_with_different_real_length[real_length_idx] ^= 1  # flip a bit from real_length
         cs1 = example_link_layer._compute_checksum(base_body)
         cs2 = example_link_layer._compute_checksum(body_with_different_real_length)
 
@@ -239,93 +242,3 @@ class TestLinkLayer:
 
         result = example_link_layer.on_receive(corrupted)
         assert result is not None
-
-# def test_transmit_frame_no_error(parity_checksum):
-#     seq = np.zeros(8, dtype=np.uint8)
-#     payload = np.array([1, 0, 1, 0], dtype=np.uint8)
-#     checksum = np.array([0, 0, 0, 0], dtype=np.uint8)
-#     transmitted_bits = np.concatenate((seq, payload, checksum))
-#     physical_layer = DummyPhysicalLayer([transmitted_bits])
-#     link = LinkLayer(physical_layer, parity_checksum, payload_size=4)
-#     frame = Frame(payload, 0, checksum)
-#     transmitted_frame = link.transmit_frame(frame)
-#     transmitted_payload = np.array([1, 0, 1, 0], dtype=np.uint8)
-#     transmitted_checksum = np.array([0, 0, 0, 0], dtype=np.uint8)
-#     transmitted_seq = 0
-#     assert transmitted_seq == transmitted_frame.get_seq()
-#     assert np.all(transmitted_payload == transmitted_frame.get_payload())
-#     assert np.all(transmitted_checksum == transmitted_frame.get_checksum())
-#     assert physical_layer.calls == 1
-#
-# def test_run_retry(parity_checksum, corrupted_bits, correct_bits):
-#     physical_layer = DummyPhysicalLayer([corrupted_bits, correct_bits])
-#
-#     link = LinkLayer(
-#         physical_layer,
-#         parity_checksum,
-#         payload_size=4,
-#         seq_size=2,
-#         checksum_size=2,
-#         max_retries=2
-#     )
-#     payload = correct_bits[2:6]
-#     frame = Frame(payload, seq=0, checksum=correct_bits[6:])
-#     received_frame = link.transmit_frame(frame)
-#
-#     assert received_frame.get_seq() == frame.get_seq()
-#     assert np.array_equal(received_frame.get_payload(), payload)
-#     assert physical_layer.calls == 2
-
-#
-# def test_payload_error_retry_success(parity_checksum):
-#     payload, seq, checksum, frame = build_link_and_frame(parity_checksum)
-#     corrupted_payload = payload.copy()
-#     corrupted_payload[2] ^= 1
-#     corrupted_bits = np.concatenate((seq, corrupted_payload, checksum))
-#     correct_bits = np.concatenate((seq, payload, checksum))
-#
-#     run_retry_test(parity_checksum, corrupted_bits, correct_bits)
-#
-# def test_seq_error_retry_success(parity_checksum):
-#     payload, seq, checksum, frame = build_link_and_frame(parity_checksum)
-#
-#     corrupted_seq = seq.copy()
-#     corrupted_seq[1] ^= 1
-#
-#     corrupted_bits = np.concatenate((corrupted_seq, payload, checksum))
-#     correct_bits = np.concatenate((seq, payload, checksum))
-#
-#     run_retry_test(parity_checksum, corrupted_bits, correct_bits)
-#
-# def test_complete_failure(parity_checksum):
-#     seq = np.zeros(2, dtype=np.uint8)
-#     payload = np.array([1, 1, 1, 0], dtype=np.uint8)
-#     checksum = np.array([0, 0], dtype=np.uint8)
-#     transmitted_bits = np.concatenate((seq, payload, checksum))
-#     physical_layer = DummyPhysicalLayer([transmitted_bits, transmitted_bits, transmitted_bits])
-#     link = LinkLayer(physical_layer, parity_checksum, payload_size=4, checksum_size=2, seq_size=2, max_retries=3)
-#     frame = Frame([0, 0, 0, 0], 0, checksum)
-#     with pytest.raises(LinkError):
-#         link.transmit_frame(frame)
-#
-# def test_multiple_blocks(parity_checksum):
-#     payload = [1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0]
-#     expected_frames = [
-#         [0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0],  # block 1 ok
-#         [0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0]  # block 2 ok
-#     ]
-#     physical_layer = DummyPhysicalLayer(expected_frames)
-#     link = LinkLayer(physical_layer, parity_checksum, payload_size=8, seq_size=2, checksum_size=2, max_retries=3)
-#     result = link.transmit(payload)
-#     assert np.all(result == payload)
-#
-# def test_block_independence(parity_checksum):
-#     payload = [1, 0, 1, 0, 1, 1, 0, 0]
-#     expected_frames = [
-#         [0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 1, 0, 1, 0, 0, 0],  # block 1 fail → retry
-#         [0, 1, 1, 1, 0, 0, 0, 0]  # block 2 ok
-#     ]
-#     physical_layer = DummyPhysicalLayer(expected_frames)
-#     link = LinkLayer(physical_layer, parity_checksum, payload_size=4, seq_size=2, checksum_size=2, max_retries=2)
-#     result = link.transmit(payload)
-#     assert np.all(result == payload)
