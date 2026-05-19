e130a1cd9cd84706b9c164dc1092018f8a47e591
Gabriel <gpena@untref.edu.ar>
2026-05-18 22:23:24 -0300

    A broken test was fixed

--- tests/integration/protocol_stack/test_protocol_stack_integration.py
+++ tests/integration/protocol_stack/test_protocol_stack_integration.py
@@ -32,5 +32,18 @@ def test_physical_has_channel_code_config():
 
 def test_stack_transmits_through_layers(link_stack):
     interface = DummyInterface()
+    link_layer = link_stack.top_layer
+    physical = link_layer.lower_layer
+
+    def transmit_with_ack(bits, iface=None):
+        interface.sent_bits = bits
+        frame = link_layer._deserialize_frame(bits)
+        ack = link_layer._build_ack(frame.get_seq())
+        ack_bits = link_layer._serialize_frame(ack)
+        link_layer.on_receive(ack_bits)
+
+    physical.transmit = transmit_with_ack
+    link_layer._validate_checksum = lambda x: True
+
     link_stack.transmit("sol", interface)
     assert interface.sent_bits is not None
